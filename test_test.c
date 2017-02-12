e3c0b8e6b277d017365e49767b250ffa31f91f1e
Brandon Tarney <brandon.tarney@gmail.com>
2017-02-12 15:14:47 -0500

    updated for library and system call

--- test_test.c
+++ test_test.c
@@ -8,12 +8,22 @@
 //#include "namespace.h"
 #include <lib.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include <string.h>
 #include <signal.h>
 
 int main()
 {
+        printf("Test system call:\n");
+
+        //Test the "test" syscall which should just print a message to stdOut
+        message m;
+        memset(&m, 0, sizeof(m));
+        (void)(_syscall(PM_PROC_NR, PM_TEST, &m));
+
+        printf("\nTest system library:\n");
         //Test the "test" syscall which should just print
-        test();
+        //test();
+        return(1);
 }
