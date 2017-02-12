d9ccffbe62ba7d08741e3ff719349b700306a2e5
Brandon Tarney <brandon.tarney@gmail.com>
2017-02-12 15:39:37 -0500

    update test_test to include what it will do

--- test_test.c
+++ test_test.c
@@ -15,15 +15,22 @@
 
 int main()
 {
-        printf("Test system call:\n");
-
+        printf("Test system call: \n");
         //Test the "test" syscall which should just print a message to stdOut
         message m;
         memset(&m, 0, sizeof(m));
         (void)(_syscall(PM_PROC_NR, PM_TEST, &m));
 
-        printf("\nTest system library:\n");
+        printf("\nTest system library call: \n");
         //Test the "test" syscall which should just print
         //test();
+
+        printf("Test kernel call: \n");
+        //new kernel call? Not sure how to directly invoke it
+
+        printf("Test system library kernel call: \n");
+        //modify "test()" to take an optional boolean input flag:
+        //Either make the system call or make the kernel call 
+
         return(1);
 }
